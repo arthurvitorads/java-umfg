fe7d64fd17da6fdb5acd0049e35744e91facef00
Arthur Vitor <109088305+arthurvitorads@users.noreply.github.com>
2026-02-27 20:29:42 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# java-umfg
+# Programação II
+
+Repositório para guardar os códigos de exemplo e/ou atividades ministradas.
---
+++ orientacao_objetos/conceitos_oo/src/net/rhuanpk/heranca/Pessoa.java
@@ -0,0 +1,36 @@
+package net.rhuanpk.heranca;
+
+import java.util.Objects;
+
+public class Pessoa {
+    protected String nome;
+    protected int idade;
+
+    public void correr() {
+        System.out.println("A pessoa " + nome + " está correndo.");
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (Objects.equals(nome, "")) {
+            System.out.println("Erro: nome não poder ser vazio!");
+            return;
+        }
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        if (idade < 0) {
+            System.out.println("Erro: idade não pode ser menor que 0!");
+        } else {
+            this.idade = idade;
+        }
+    }
+}
